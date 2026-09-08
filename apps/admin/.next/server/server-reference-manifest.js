5358a535733597ba3901d9335f0c38d8ab0d1b39
Jyphra Technology <dev@jyphra.com>
2026-09-09 02:56:46 +0545

    chore(build): track pnpm-lock.yaml and ignore .next build artifacts

--- apps/admin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"nLcNWYx76BHZ0o3shyYE9++7CyfbkUZesqdJCLZWyg0=\"}"
--- apps/admin/.next/static/chunks/app/page-419662d8d92eb605.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4472:function(){}},function(n){n.O(0,[699,629,744],function(){return n(n.s=4472)}),_N_E=n.O()}]);
